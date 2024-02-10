f2b75798e39cb4f3245b4ada183b2c986b74e865
Isacco-B <git@isaccobertoli.com>
2024-02-10 19:15:14 +0100

    complete Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <div className="max-w-[1070px] py-6 mx-auto">
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-lg md:text-xl font-light">
+          Isacco Bertoli Start2Impact
+        </p>
+        <p className="text-lg md:text-xl">
+          Copyright © 2024 Make It Dairy Free
+        </p>
+      </div>
+    </div>
+  );
+}
