d50ef23d4f9160b409df6085fc643d3839a7410c
Isacco-B <git@isaccobertoli.com>
2024-02-10 19:13:40 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+//Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import RecipePage from "./pages/RecipePage";
+import NotFound from "./pages/NotFound";
+import Subscribe from "./pages/Subscribe";
+
+
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/subscribe" element={<Subscribe />} />
+        <Route path="/recipe/:recipeId" element={<RecipePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
